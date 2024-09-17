9c2ab29e66529c65cdfe2c3a4ef5d6ee9d4c5818
Mr. Mueller <bmueller@hstat.org>
2024-09-17 15:38:46 -0400

    add writeup generator script

---
+++ generate.sh
@@ -0,0 +1,9 @@
+# once template is ready, this will duplicate the template to generate the correctly-named writeups
+# use `sh generate.sh` to run (or just copy/paste the code below)
+cp template.md 01-javascript/js-basics-writeup.md
+cp template.md 01-javascript/arrays-writeup.md
+cp template.md 01-javascript/conditionals-writeup.md
+cp template.md 01-javascript/functions-writeup.md
+cp template.md 01-javascript/loops-writeup.md
+cp template.md 02-dom/dom-writeup.md
+cp template.md 03-p5js/p5js-writeup.md
