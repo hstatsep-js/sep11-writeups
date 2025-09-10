edacedb9b8a3dd7e5947ed0f6ba6554f0ddf67ae
Mr. Mueller <bmueller2@schools.nyc.gov>
2025-09-10 16:21:51 -0400

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -2,8 +2,8 @@
 # use `sh generate.sh` to run (or just copy/paste the code below)
 cp template.md 01-javascript/js-basics-writeup.md
 cp template.md 01-javascript/arrays-writeup.md
-cp template.md 01-javascript/conditionals-writeup.md
 cp template.md 01-javascript/functions-writeup.md
+cp template.md 01-javascript/conditionals-writeup.md
 cp template.md 01-javascript/loops-writeup.md
 cp template.md 02-dom/dom-writeup.md
 cp template.md 03-p5js/p5js-writeup.md
